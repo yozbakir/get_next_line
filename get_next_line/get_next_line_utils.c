38a219fcad27f7e78eb98ffc07d4dc2bf5f25044
kolxnein <115457760+kolxnein@users.noreply.github.com>
2023-12-24 17:07:21 +0300

    Add files via upload

---
+++ get_next_line/get_next_line_utils.c
@@ -0,0 +1,97 @@
+#include "get_next_line.h"
+#include <unistd.h> 
+
+size_t	ft_strlen(const char *s)
+{
+	size_t	i;
+
+	i = 0;
+	while (s[i] != '\0')
+		i++;
+	return (i);
+}
+
+char	*ft_strdup(const char *buffer)
+{
+	char	*stack;
+	int		i;
+
+	stack = (char *)malloc(ft_strlen(buffer) + 1);
+	if (!stack)
+		return (0);
+    i = 0;
+	while (buffer[i])
+    {
+		stack[i] = buffer[i];
+        i++;
+    }
+	stack[i] = '\0';
+	return (stack);
+}
+
+char	*ft_strjoin(char const *stack, char const *buffer)
+{
+	size_t	i;
+	char	*new_str;
+
+	if (!stack || !buffer)
+		return (0);
+	new_str = (char *)malloc(ft_strlen(stack) + ft_strlen(buffer) + 1);
+	if (!new_str)
+		return (NULL);
+	i = 0;
+	while (*stack)
+		new_str[i++] = *stack++;
+	while (*buffer)
+		new_str[i++] = *buffer++;
+	new_str[i] = '\0';
+	return (new_str);
+}
+
+char	*ft_substr(char const *stack, unsigned int start, size_t len)
+{
+	char	*new_line;
+	size_t	i;
+	size_t	j;
+
+	if (!stack || len <= 0)
+		return (0);
+	new_line = (char *)malloc(sizeof(char) * (len + 1));
+	if (!new_line)
+		return (0);
+	i = start;
+	j = 0;
+	while (i < len && j < len)
+	{
+		new_line[j] = stack[i];
+		i++;
+		j++;
+	}
+	new_line[j] = '\0';
+	return (new_line);
+}
+
+void	*ft_free_stack(char **stack, int create_line)
+{
+	char	*line;
+
+	if (!*stack)
+		return (0);
+	if (!create_line)
+	{
+		if (*stack)
+		{
+			free(*stack);
+			*stack = 0;
+		}
+		return (0);
+	}
+	else if (create_line)
+	{
+		line = ft_strdup(*stack);
+		free(*stack);
+		*stack = 0;
+		return (line);
+	}
+	return (0);
+}
